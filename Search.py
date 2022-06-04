e705786b2d2eb59093e324b6153b31aebbee3195
whymeeverytime <borgahaktanbilen01@gmail.com>
2022-06-04 22:52:06 +0300

    Aurelion Sol Bug fix

--- Search.py
+++ Search.py
@@ -2,6 +2,9 @@ import requests
 
 
 def search(champion):
+    if champion == "aurelion-sol":
+        champion = "aurelion-sol-130"
+
     url = f"https://www.mobafire.com/league-of-legends/champion/{champion}"
     result = requests.get(url)
     if result.status_code != 200:
